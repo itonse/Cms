c6babe584f56423f1e9fb973a2f0503fcdcee93b
itonse <govl3dnjs@gmail.com>
2023-06-16 13:58:01 +0900

    feat : [006-LOGIN_SELLER] 셀러 로그인 구현
    로그인 후 토큰 발행

--- user-api/src/main/java/com/itonse/cms/user/application/SignInApplication.java
+++ user-api/src/main/java/com/itonse/cms/user/application/SignInApplication.java
@@ -4,8 +4,10 @@ import com.itonse.cms.domain.config.JwtAuthenticationProvider;
 import com.itonse.cms.domain.domain.common.UserType;
 import com.itonse.cms.user.domain.SignInForm;
 import com.itonse.cms.user.domain.model.Customer;
+import com.itonse.cms.user.domain.model.Seller;
 import com.itonse.cms.user.exception.CustomException;
 import com.itonse.cms.user.service.customer.CustomerService;
+import com.itonse.cms.user.service.seller.SellerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,16 +18,24 @@ import static com.itonse.cms.user.exception.ErrorCode.LOGIN_CHECK_FAIL;
 public class SignInApplication {
 
     private final CustomerService customerService;
+    private final SellerService sellerService;
     private final JwtAuthenticationProvider provider;
 
     public String customerLoginToken(SignInForm form) {
         // 1. 로그인 가능 여부
         Customer c = customerService.findValidCustomer(form.getEmail(), form.getPassword())
                 .orElseThrow(() -> new CustomException(LOGIN_CHECK_FAIL));
-        // 2. 토큰을 발행하고
+
+        // 2. 토큰을 발행 후 response
         return provider.createToken(c.getEmail(), c.getId(), UserType.CUSTOMER);
+    }
 
-        // 3. 토큰을 response 한다.
+    public String sellerLoginToken(SignInForm form) {
+        // 1. 로그인 가능 여부
+        Seller s = sellerService.findValidSeller(form.getEmail(), form.getPassword())
+                .orElseThrow(() -> new CustomException(LOGIN_CHECK_FAIL));
 
+        // 2. 토큰을 발행 후 response
+        return provider.createToken(s.getEmail(), s.getId(), UserType.SELLER);
     }
 }
